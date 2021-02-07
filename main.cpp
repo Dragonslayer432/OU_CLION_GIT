5d5cbe76015132dd719fb95f07cecc1caf71c9a0
Dragonslayer432 <krongjacobs@gmail.com>
2021-02-07 22:01:59 +0100

    Zmiany dodatkowe w 03

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+Init Policz (int a, int b){
+    Return (a+b)*(a-b);
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std:: cout <<"W pierwszym branchu; 2+3="<< Policz(2,3) <<std::endl;
     return 0;
 }
+
+
